9313c1cba64959b0bf5a72cf54c350a87cdc2141
spaceyyy <mickey_bachhal@hotmail.com>
2023-07-22 01:15:43 -0500

    added multiple files; added multiple files to t-o-c.html

---
+++ headfirst/HTML5/Chapter4_solo/constructor.js
@@ -0,0 +1,22 @@
+/* constructor.js */
+
+function Dog(name, breed, weight) {
+    this.name = name;
+    this.breed = breed;
+    this.weight = weight;
+    this.bark = function() {
+        if (this.weight > 25) {
+            alert(this.name + " says Woof!");
+        } else {
+            alert(this.name + " says Yip!");
+        }
+    };
+}
+
+var fido = new Dog("Fido", "Mixed", 38);
+var tiny = new Dog("Tiny", "Chawalla", 8);
+var clifford = new Dog("Clifford", "Bloodhound", 65);
+
+fido.bark();
+tiny.bark();
+clifford.bark();
